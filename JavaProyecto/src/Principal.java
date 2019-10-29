d0b40e18d0bbea114cfc2ca45e9b5cbd1440bd36
MIKE224 <miguel@siemprendedor.com>
2019-10-29 12:52:48 +0100

    Actu1

--- JavaProyecto/src/Principal.java
+++ JavaProyecto/src/Principal.java
@@ -3,5 +3,7 @@ public class Principal {
     public static void main(String[] args) {
         String nombre="ferran";
         System.out.println("Tu nombre es: "+nombre);
+        String nombre1="mike";
+        System.out.println("Tu nombre es: "+nombre1);
     }
 }
